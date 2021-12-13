42c4508dfe80c3dffdd0b1618b8134f5304051ec
arnaud2210 <adjimonarnaud@gmail.com>
2021-12-13 23:05:30 +0000

    main

--- Plateforme_inscription/View/Publication.php
+++ Plateforme_inscription/View/Publication.php
@@ -21,7 +21,7 @@
                     <div class="card-body">
                         <h5 class="card-title">Special title treatment</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn btn-outline-secondary">Go somewhere</a>
                     </div>
                 </div>
             </div>
---
+++ Plateforme_inscription/View/database/connexion.php
@@ -0,0 +1,11 @@
+<?php
+
+    try 
+    {
+        $db = new PDO("mysql:host=localhost;dbname=concours;charset=utf8","root","",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        echo "connexion réussie";
+    } 
+    catch (\Throwable $th) 
+    {
+        die("Erreur de connexion : ".$th->getMessage());
+    }

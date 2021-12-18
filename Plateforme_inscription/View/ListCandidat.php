4bc4515bb3406c414c1f06e9a95df5d23171845d
arnaud2210 <adjimonarnaud@gmail.com>
2021-12-18 20:03:11 +0000

    new_commit

---
+++ Plateforme_inscription/View/ListCandidat.php
@@ -0,0 +1,74 @@
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+<?php
+    require('database/connexion.php');
+    $table = $db->query('select* FROM v_candidature');
+?>
+<section class="note py-5">
+      <div class="container">
+        <h2 align="center">LISTE DES CANDIDATURES</h2>
+        <hr>
+        <form method="POST" action="">
+          <div class="row">
+            <div class="col">
+              <section class="">
+                <div>
+                  <table class="table table-hover table-bordered">
+                  <thead>
+                    <tr class="bg-warning">
+                      <th scope="col">N°</th>
+                      <th scope="col">Nom</th>
+                      <th scope="col">Prenom</th>
+                      <th scope="col">Sexe</th>
+                      <th scope="col">Naissance</th>
+                      <th scope="col">Nationnalité</th>
+                      <th scope="col">Série</th>
+                      <th scope="col">Session</th>
+                      <th class="bg-secondary" align="center" scope="col"></th>
+                      <!--?php if ($_SESSION['niv'] == 0) 
+                        {
+                      ?-->
+                      <!--th scope="col" class="bg-secondary"></th-->
+                      <!--th scope="col" class="bg-success"></th-->
+                      <!--?php 
+                        }
+                      ?-->
+                    </tr>
+                  </thead>
+                  <tbody>
+                      <?php 
+                        while ($post = $table->fetch())
+                        { 
+                      ?>  <tr>
+                            <!--th scope="row"><!--?php echo htmlspecialchars($post['matricule']); ?></th-->
+                            <td scope="row"><strong><?php echo htmlspecialchars($post['numcandidat']); ?></strong></td>
+                            <td><?php echo htmlspecialchars($post['nom']); ?></td>
+                            <td><?php echo htmlspecialchars($post['prenom']); ?></td>
+                            <td><?php echo htmlspecialchars($post['sexe']); ?></td>
+                            <td><?php echo htmlspecialchars($post['naissance']); ?></td>
+                            <td><?php echo htmlspecialchars($post['nationnalité']); ?></td>
+                            <td><?php echo htmlspecialchars($post['libserie']); ?></td>
+                            <td><?php echo htmlspecialchars($post['bacsession']); ?></td>
+                            <td align="center">
+                              <a class="btn btn-outline-danger" href="database/telecharger.php?num=<?php echo $post['numfic'];?>">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                </svg>
+                                Télécharger Att-n°<?php echo htmlspecialchars($post['numfic']); ?>
+                              </a>
+                            </td>
+                            
+                          </tr>
+                      <?php
+                        }  
+                          $table->closeCursor();
+                      ?>  
+                  </tbody>
+                </table>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </form>
+</section>
--- Plateforme_inscription/View/Navbar.php
+++ Plateforme_inscription/View/Navbar.php
@@ -9,7 +9,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-success bg-success">
             <div class="container">
-                <a class="navbar-brand text-light" href="#"><strong>IAI-TOGO</strong></a>
+                <a class="navbar-brand text-light" href="index.php"><strong>IAI-TOGO</strong></a>
                 <button class="navbar-toggler" type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent"
@@ -22,22 +22,28 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active text-light" aria-current="page"
-                                href="Accueil.php">Présentation</a>
+                                href="index.php">Présentation</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-light" href="Publication.php">Communiqué</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled text-light" href=""></a>
+                            <a class="nav-link text-light" href="Classement.php">Rechercher</a>
                         </li>
                     </ul>
 
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a href="" class="nav-link text-light"></a>
+                            <a href="Affichage.php" class="nav-link text-light">Voir Liste</a>
                         </li>
                         <li class="nav-item">
-                            <a class="btn btn-warning " href="Formulaire.php">S'inscrire</a>
+                            <a class="btn btn-warning " href="Formulaire.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                                </svg>
+                                S'inscrire
+                            </a>
                         </li>
                     </ul>
 
